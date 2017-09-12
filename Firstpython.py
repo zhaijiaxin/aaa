a84aa98247f7a09b363badbee3176fe72db24826
zhaijiaxin <2742089569@qq.com>
2017-09-12 15:49:36 +0800

    pythonpro

---
+++ Firstpython.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+text = '你好 杨佳佳'
+print text
+print ("哈哈")
+# 这里的text就是utf-8编码u
+
+#联系多行代码
+str1="yang"
+str2="jiajia"
+str3="jia"
+temp=str1+str2+str3
+print (temp)
+
+#python的数据leixin
+num=1000
+strhe="haha"
+strhel='wobuhao' #string 单双引号不分都是字符串
+fff=100.1
+print (num);print (strhe); print(strhe);print (strhel)
+
+print '\n'.join([' '.join(['%s*%s=%-2s' % (y,x,x*y) for y in range(1,x+1)]) for x in range(1,10)])
+
